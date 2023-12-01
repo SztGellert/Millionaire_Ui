2555bc873573df7774955e7cff2799ff521c303b
SztGellert <sztankgellert@gmail.com>
2023-12-01 05:25:36 +0100

    implemented basic email feedback feature

--- millionaire-ui/src/app/quiz/quiz.service.ts
+++ millionaire-ui/src/app/quiz/quiz.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Subject} from "rxjs";
 
 export interface Question {
@@ -9,25 +9,39 @@ export interface Question {
   topic: string;
   difficulty: boolean;
 }
+
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
+  quizzesChanged: Subject<Question[]> = new Subject<Question[]>;
+
   constructor(private http: HttpClient) {
   }
-  quizzesChanged: Subject<Question[]> = new Subject<Question[]>;
 
   fetchQuiz(topic: string, difficulty: string) {
 
-    if (topic == "all") { topic = "" }
-    if (difficulty == "all") { difficulty = ""}
+    if (topic == "all") {
+      topic = ""
+    }
+    if (difficulty == "all") {
+      difficulty = ""
+    }
 
     this.http.get<Question[]>('https://yi4tfqk2xmyzsgt72ojur5bk6q0mjtnw.lambda-url.eu-north-1.on.aws?topic=' + topic + '&difficulty=' + difficulty)
       .subscribe(resData => this.quizzesChanged.next(resData));
   }
+
+  sendFeedbackEmail(data: any) {
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    this.http.post('https://formspree.io/f/mdorryrg',
+      {name: data.name, replyto: data.email, message: data.messages},
+      {'headers': headers}).subscribe(
+      response => {
+        console.log(response);
+      }
+    );
+  }
+
 }
-
-
-
-

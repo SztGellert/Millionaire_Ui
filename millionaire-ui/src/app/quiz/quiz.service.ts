375d6e8a8a0306f8b517ad545d53898555d1178b
SztGellert <sztankgellert@gmail.com>
2024-02-21 03:22:05 +0100

    align post request parameters

--- millionaire-ui/src/app/quiz/quiz.service.ts
+++ millionaire-ui/src/app/quiz/quiz.service.ts
@@ -37,7 +37,6 @@ export class QuizService {
     }
 
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-
     this.http.post<Question[]>('https://yi4tfqk2xmyzsgt72ojur5bk6q0mjtnw.lambda-url.eu-north-1.on.aws?topic=' + topic + '&difficulty=' + difficulty, exceptions, {'headers': headers})
       .subscribe(resData => this.quizzesChanged.next(resData));
   }
